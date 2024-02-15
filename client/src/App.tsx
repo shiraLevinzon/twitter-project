955bcabca0c07fdd55229affafac0fb238dc7642
shiraLevinzon <levinzon@g.jct.ac.il>
2024-02-15 19:17:06 +0200

    🗑️

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,13 @@
-import { createContext, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from './context/UserContext';
 import {TweetProvider} from './context/TweetContext';
 import AppRoutes from './routes/AppRoutes';
-import UserDocument from '../../types/user.type';
-import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 
 const queryClient: QueryClient = new QueryClient();
 
 function App() {
-
-  const [tweet, setTweet] = useState({});
-  const [user, setUser] = useState({});
-
 
   return (
     <QueryClientProvider client={queryClient}>
